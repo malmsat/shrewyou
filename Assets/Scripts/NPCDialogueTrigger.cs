ea17bb27e422650c56fe4e6d4cf223e7a17d14f9
jabbahut69 <satu.malmberg.satu@gmail.com>
2025-02-13 14:37:13 +0200

    Add NPC dialogue UI

---
+++ Assets/Scripts/NPCDialogueTrigger.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class NPCDialogueTrigger : MonoBehaviour
+{
+    public GameObject dialogueUI; // Assign in Inspector (Canvas Panel)
+    public TextMeshProUGUI dialogueText; // Assign in Inspector
+    public Button option1Button; // Assign in Inspector
+    public Button option2Button; // Assign in Inspector
+
+    private bool isPlayerInRange = false;
+
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            ShowDialogue();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            HideDialogue();
+        }
+    }
+
+private bool isDialogueActive = false;
+
+void ShowDialogue()
+{
+    dialogueUI.SetActive(true);
+    isDialogueActive = true;
+
+    Cursor.lockState = CursorLockMode.None;
+    Cursor.visible = true;
+}
+
+void HideDialogue()
+{
+    dialogueUI.SetActive(false);
+    isDialogueActive = false;
+
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+}
+
+void Update()
+{
+    if (isDialogueActive)
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
+
+
+
+    void ChooseOption(int option)
+    {
+        if (option == 1)
+        {
+            dialogueText.text = "Knowledge is a powerful thing.";
+        }
+        else if (option == 2)
+        {
+            dialogueText.text = "Wealth can be deceiving.";
+        }
+
+        option1Button.gameObject.SetActive(false);
+        option2Button.gameObject.SetActive(false);
+
+        Invoke("HideDialogue", 2f);
+    }
+}
